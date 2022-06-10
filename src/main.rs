deda1f07eb558b7d02413e0c04c2bcbfa7205874
Jonatas Luiz de Oliveira <contact@jonatasoliveira.dev>
2022-06-10 09:38:36 -0300

    create modules

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Local, Utc};
+use clap::{App, Arg, ArgEnum, Parser, SubCommand};
 use config::Config;
 use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about = "Clockify CLI for terminal")]
+pub struct Cli {
+    #[clap(short, long)]
+    track_mode: String,
+}
 
 #[allow(unused)]
 pub struct StartTracker {
@@ -20,21 +29,27 @@ pub struct EndTracker {
 #[allow(unused)]
 mod tracker {
     use chrono::{DateTime, Utc};
-    fn start_track<UTC: chrono::TimeZone>(start_date: DateTime<UTC>) -> String {
-        String::from("Stop Date")
+    pub fn start_track<UTC: chrono::TimeZone>(start_date: DateTime<UTC>) {
+        println!("Start Date: ");
     }
-    fn stop_track<UTC: chrono::TimeZone>(stop_date: DateTime<UTC>) -> String {
+    pub fn stop_track<UTC: chrono::TimeZone>(stop_date: DateTime<UTC>) {
         // let client = reqwest::Client::new();
         // let res = client
         //     .post("http://httpbin.org/post")
         //     .body("the exact body that is sent")
         //     .send()
         //     .await?;
-        String::from("Start Date")
+        println!("Stop Date: ");
     }
 }
 
 fn main() {
+    // let args = Cli {
+    //     mode: track_mode,
+    //     time: track_time,
+    // };
+    let tracker_mode = std::env::args().nth(1).expect("Track Mode");
+    let args = Cli::parse();
     let settings = Config::builder()
         // Add in `./Settings.toml`
         .add_source(config::File::with_name("Settings"))
@@ -47,5 +62,12 @@ fn main() {
     let config_local = settings
         .try_deserialize::<HashMap<String, String>>()
         .unwrap();
-    println!("{:?}", config_local.get("BASE_URL"))
+    println!("{:?}", config_local.get("BASE_URL"));
+    println!("{:?}", args.track_mode);
+    match &*args.track_mode {
+        "start" => tracker::start_track(Local::now()),
+        "stop" => tracker::stop_track(Local::now()),
+        _ => panic!("Option is not valid!"),
+    }
+    ()
 }
